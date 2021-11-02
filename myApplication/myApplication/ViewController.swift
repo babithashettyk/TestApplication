2acc9453cb7dfdb6c112176abc22ebedf45967cb
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-02 16:54:18 +0530

    add frame

---
+++ myApplication/myApplication/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myApplication
+//
+//  Created by Babitha Shetty K on 02/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
